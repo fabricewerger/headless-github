e4c731eb712f7693a292db55f56968bbee1b9d9a
Nathan Clare <n.clare@youweagency.com>
2023-10-31 21:44:10 +0100

    INTAHLA-10: Add webpack rule to apply custom SVGO transformation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 
 const nextConfig = {
   reactStrictMode: true,
@@ -16,7 +17,22 @@ const nextConfig = {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
         resourceQuery: { not: /url/ },
-        use: ['@svgr/webpack'],
+        include: [path.resolve(__dirname, 'public/assets/icons')],
+        use: {
+          loader: '@svgr/webpack',
+          options: {
+            configFile: 'public/assets/icons/svgo.config.js',
+          },
+        },
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ },
+        exclude: [path.resolve(__dirname, 'public/assets/icons')],
+        use: {
+          loader: '@svgr/webpack',
+        },
       }
     )
     fileLoaderRule.exclude = /\.svg$/i
